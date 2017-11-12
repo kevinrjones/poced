5672e254f89fce6e947e6ea64ff5ccfb36da859d
Kevin <kevin@rocksolidknowledge.com>
2017-11-12 18:08:43 +0000

    Added initial Owin configuration

---
+++ pocedweb/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(pocedweb.Startup))]
+
+namespace pocedweb
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            Console.WriteLine();
+        }
+    }
+}
