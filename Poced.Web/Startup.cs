00ec32e0f7a3d20394597ef155afed4b7d97c6df
Kevin <kevin@rocksolidknowledge.com>
2018-01-13 09:01:42 +0000

    Added cookie code and registration placeholder to project

--- Poced.Web/Startup.cs
+++ Poced.Web/Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -51,6 +53,21 @@ namespace poced.web
                 options.User.RequireUniqueEmail = true;
             });
 
+            /*
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {                
+                AuthenticationType = "Cookie",
+                LoginPath = new PathString("/login")
+            });
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ExternalCookie",
+                AuthenticationMode = AuthenticationMode.Passive,
+                ExpireTimeSpan = TimeSpan.FromMinutes(5)
+            });
+
+            */
             services.ConfigureApplicationCookie(options =>
             {
                 // Cookie settings
@@ -61,6 +78,20 @@ namespace poced.web
                 options.AccessDeniedPath = "/Account/AccessDenied"; // If the AccessDeniedPath is not set here, ASP.NET Core will default to /Account/AccessDenied
                 options.SlidingExpiration = true;
             });
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/Account/Login";
+                    options.LogoutPath = "/Account/Logout";
+                })
+                .AddGoogle(options =>
+                {
+                    //googleOptions.ClientId = Configuration["Authentication:Google:ClientId"];
+                    //googleOptions.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+                    options.ClientId = "656376280253-dj3e3qqhmbcp4oir5gfk8btm17v8b9om.apps.googleusercontent.com";
+                    options.ClientSecret = "HvijClsL9XzNeVICelRuWc5W";
+                });
 
             services.AddMvc();
         }
@@ -80,7 +111,6 @@ namespace poced.web
 
             app.UseAuthentication();
 
-
             app.UseStaticFiles();
 
             app.UseMvc(routes =>
@@ -90,5 +120,49 @@ namespace poced.web
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+
+        /*
+                 private void RegisterTypes(ContainerBuilder builder)
+        {
+            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var perfLogger = new LoggerConfiguration()
+                .ReadFrom.AppSettings()
+                .CreateLogger();
+
+            var usageLogger = new LoggerConfiguration()
+                .ReadFrom.AppSettings()
+                .WriteTo.File($@"{baseDirectory}\logs\usage.log")
+                .CreateLogger();
+
+            var errorLogger = new LoggerConfiguration()
+                .ReadFrom.AppSettings()
+                .CreateLogger();
+
+            var diagnosticLogger = new LoggerConfiguration()
+                .ReadFrom.AppSettings()
+                .CreateLogger();
+
+            var connectionString = ConfigurationManager.ConnectionStrings["pocedEntities"].ConnectionString;
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            var repositoryAssemblies = Assembly.Load("Poced.Services");
+            builder.RegisterAssemblyTypes(repositoryAssemblies).AsImplementedInterfaces();
+
+            builder.RegisterType<ArticlesRepository>().As<IArticlesRepository>().WithParameter(new NamedParameter("connectionString", connectionString));
+            builder.RegisterType<UsersRepository>().As<IUsersRepository>().WithParameter(new NamedParameter("connectionString", connectionString));
+            builder.RegisterType<AppConfiguration>().As<IConfiguration>();
+
+
+            // todo: autofac named parameters with registered values
+            builder.RegisterType<PocedSerlogLogger>().As<IPocedLogger>().WithParameter(new NamedParameter("perfLogger", perfLogger))
+                .WithParameter(new NamedParameter("usageLogger", usageLogger))
+                .WithParameter(new NamedParameter("errorLogger", errorLogger))
+                .WithParameter(new NamedParameter("diagnosticLogger", diagnosticLogger))
+                .SingleInstance();
+
+            builder.RegisterFilterProvider();
+        }
+
+         */
     }
 }
